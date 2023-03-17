de2109e14c031a0b538232964ef9820ece716976
Andy Davis <andydavis6055@gmail.com>
2023-03-17 17:23:09 -0600

    all passing

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+  function scuberGreetingForFeet (feet) {
+    if (feet <= 400) {
+      return 'This one is on me!';
+    } else if (feet <= 2000) {
+      return 'That will be twenty bucks.';
+    } else if (2000 < feet && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    }
+  }
+function ternaryCheckCity(theBigCheck) {
+  return (theBigCheck === "NYC") ? "Ok, sounds good." : "No go."
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(response){
+  switch (response){
+    case "generous":
+      return "Thank you so much."
+      break
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }
 }
